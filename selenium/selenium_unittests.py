e55388110dd9f5b5551bd8cd9e082ebcae257311
Adrien VERGNAUD <a.vergnaud@cat-amania.com>
2024-08-27 11:26:27 +0200

    suite

--- selenium/selenium_unittests.py
+++ selenium/selenium_unittests.py
@@ -4,18 +4,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-driver = webdriver.Firefox()
+# driver = webdriver.Firefox()
+options = webdriver.FirefoxOptions()
+server = 'http://127.0.0.1:4444/wd/hub'
+
+driver = webdriver.Remote(command_executor=server, options=options)
+
 driver.get("http://localhost:3000")
 
 print(driver.title)
 
 # https://www.selenium.dev/selenium/docs/api/py/webdriver_support/selenium.webdriver.support.expected_conditions.html
-try:
-    fcp_value_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "fcp-value"))
-    )
-    print(fcp_value_element.text) 
-except NoSuchElementException:
-    print("fcp-value NOT found") 
 
-driver.close()
+fcp_value_element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "fcp-value"))
+)
+print(fcp_value_element.text) 
+
+driver.quit()
