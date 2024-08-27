6b7cb8341b2d2db0a426066a5aee98e331586b07
Adrien VERGNAUD <a.vergnaud@cat-amania.com>
2024-08-27 17:10:29 +0200

    test

--- selenium/selenium_unittests.py
+++ selenium/selenium_unittests.py
@@ -8,7 +8,9 @@ options = webdriver.FirefoxOptions()
 server = 'http://127.0.0.1:4444/wd/hub'
 
 driver = webdriver.Remote(command_executor=server, options=options)
-driver.get("http://localhost:3000")
+# driver.get("http://localhost:3000")
+# driver.get("http://207.154.209.99/")
+driver.get("http://207.154.209.99:81/")
 fcp_value_element = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, "100_title"))
 )
--- selenium/start-selenium-tests.ps1
+++ selenium/start-selenium-tests.ps1
@@ -1,5 +1,5 @@
 $i=1
-for(;$i -le 20;$i++)
+for(;$i -le 40;$i++)
 {
 	Start-Process -NoNewWindow python .\selenium_unittests.py
 	Start-Sleep -Seconds 1
